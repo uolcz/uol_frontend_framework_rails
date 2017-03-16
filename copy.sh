c72fe3d2d00772ad9453ffd882e83ab6fce3dec2
Premysl Donat <pdonat@seznam.cz>
2017-03-16 12:41:36 +0100

    Update from uol_frontend_framework

---
+++ copy.sh
@@ -0,0 +1,2 @@
+cp ../../html_js/uol-frontend-framework/dist/js/bundle.js vendor/assets/javascripts/bundle.js;
+cp ../../html_js/uol-frontend-framework/src/styles/* vendor/assets/stylesheets/ -r;
--- lib/uol_frontend_framework_rails.rb
+++ lib/uol_frontend_framework_rails.rb
@@ -3,6 +3,11 @@ require "uol_frontend_framework_rails/version"
 module UolFrontendFrameworkRails
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'uol_frontend_framework.assets' do |app|
+        %w(stylesheets javascripts).each do |sub|
+          app.config.assets.paths << root.join('assets', sub).to_s
+        end
+      end
     end
   end
 end
