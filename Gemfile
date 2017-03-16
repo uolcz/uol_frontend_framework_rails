86a7249bf6d3478166a93436d3618656d6afe7ea
Premysl Donat <pdonat@seznam.cz>
2017-03-16 10:44:26 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in uol_frontend_framework_rails.gemspec
+gemspec
---
+++ lib/uol_frontend_framework_rails.rb
@@ -0,0 +1,5 @@
+require "uol_frontend_framework_rails/version"
+
+module UolFrontendFrameworkRails
+  # Your code goes here...
+end
